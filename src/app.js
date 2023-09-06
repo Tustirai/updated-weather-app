5d03cfb70f06bc4bceb3aa68adaf7771524050e1
Audrey Huvaya <135869085+Tustirai@users.noreply.github.com>
2023-09-06 09:39:27 +0200

    updated search form

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-function updateDate(timestamp) {
-  let date = new Date(timestamp * 1000);
+function updateDate(time) {
+  let date = new Date(time * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let months = [
@@ -28,13 +28,50 @@ function updateDate(timestamp) {
   ).innerHTML = `${currentMonth} | ${days[day]} | ${currentTime} `;
 }
 
-function updateLocation(response) {
-  document.querySelector("#live-City").innerHTML = response.data.city;
-  updateDate(response.data.dt);
-  console.log(response);
+function updateDateBySearch(response) {
+  let newTime = response.data.time;
+  let date = new Date(newTime * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  let currentTime = hours + ":" + minutes + " " + ampm;
+  let currentMonth = months[date.getMonth()];
+  document.querySelector(
+    "#live-date"
+  ).innerHTML = `${currentMonth} | ${days[day]} | ${currentTime} `;
 }
 
-let livelocation = document.querySelector("#location-search");
+let key = "9ca6fftf31a653429384425b05bobb8e";
+let query = document.querySelector(".searchInput").value;
+let units = "metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=${units}`;
+axios.get(apiUrl).then((response) => {
+  updateDateBySearch(response);
+});
+
+function updateLocation(response) {
+  document.querySelector("#live-City").innerHTML = response.data.city;
+}
+
+let livelocation = document.querySelector("#live-location-search");
 livelocation.addEventListener("click", function () {
   navigator.geolocation.getCurrentPosition(function (position) {
     let lat = position.coords.latitude;
@@ -49,12 +86,24 @@ livelocation.addEventListener("click", function () {
   });
 });
 
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let query = document.querySelector(".searchInput").value;
+  let key = "9ca6fftf31a653429384425b05bobb8e";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=${units}`;
+  axios.get(apiUrl).then(function (response) {
+    updateLocation(response);
+    updateDateBySearch(response);
+  });
+});
+
 function showJohannesburg() {
   let query = "Johannesburg";
   let key = "9ca6fftf31a653429384425b05bobb8e";
   let units = "metric";
   let url = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=${units}`;
-
   axios.get(url).then(function (response) {
     updateLocation(response);
     updateDate(response.data.time);
