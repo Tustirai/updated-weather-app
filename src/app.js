3d8edc7dc6891c618ca7fde8c72b1625a8d60062
Audrey Huvaya <135869085+Tustirai@users.noreply.github.com>
2023-09-07 11:20:21 +0200

    Updated JS
    update icon, description, weather conditions based on API response.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 function updateDate(time) {
   let date = new Date(time * 1000);
-  let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let months = [
     "Jan",
@@ -16,37 +15,7 @@ function updateDate(time) {
     "Nov",
     "Dec",
   ];
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let ampm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  let currentTime = hours + ":" + minutes + " " + ampm;
-  let currentMonth = months[date.getMonth()];
-  document.querySelector(
-    "#live-date"
-  ).innerHTML = `${currentMonth} | ${days[day]} | ${currentTime} `;
-}
-
-function updateDateBySearch(response) {
-  let newTime = response.data.time;
-  let date = new Date(newTime * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let ampm = hours >= 12 ? "PM" : "AM";
@@ -58,17 +27,32 @@ function updateDateBySearch(response) {
     "#live-date"
   ).innerHTML = `${currentMonth} | ${days[day]} | ${currentTime} `;
 }
-
-let key = "9ca6fftf31a653429384425b05bobb8e";
-let query = document.querySelector(".searchInput").value;
-let units = "metric";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=${units}`;
-axios.get(apiUrl).then((response) => {
-  updateDateBySearch(response);
-});
 
 function updateLocation(response) {
+  console.log(response);
   document.querySelector("#live-City").innerHTML = response.data.city;
+
+  let temperature = Math.round(response.data.temperature.current);
+  document.querySelector("#live-Temp").innerHTML = `${temperature}`;
+
+  document.querySelector("#live-description").innerHTML =
+    response.data.condition.description.charAt(0).toUpperCase() +
+    response.data.condition.description.slice(1);
+
+  document.querySelector(
+    "#liveHumidity"
+  ).innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
+
+  document.querySelector(
+    "#liveWind"
+  ).innerHTML = `Wind: ${response.data.wind.speed} m/s`;
+
+  let iconElement = document.querySelector("#live-weather-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.icon);
 }
 
 let livelocation = document.querySelector("#live-location-search");
@@ -95,7 +79,7 @@ searchForm.addEventListener("submit", function (event) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=${units}`;
   axios.get(apiUrl).then(function (response) {
     updateLocation(response);
-    updateDateBySearch(response);
+    updateDate(response);
   });
 });
 
